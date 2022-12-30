21f0c4cdb71b8b211244d051709f19a4db7c3e93
msaqibdev <msaqibdev@gmail.com>
2022-12-30 18:29:43 +0500

    update#01

--- context/audioprovider.js
+++ context/audioprovider.js
@@ -1,77 +1,77 @@
 import React, { useEffect, createContext, useState } from "react";
 import { Alert } from "react-native";
-import * as MediaLibrary from 'expo-media-library'
+import * as MediaLibrary from "expo-media-library";
 
-
-export const AudioContext = createContext()
+export const AudioContext = createContext();
 
 const AudioPlayer = ({ children }) => {
-    const [audioFiles, setAudioFiles ] = useState([]);
+  const [audioFiles, setAudioFiles] = useState([]);
 
-    useEffect(() => {
-        getPermissions();
-    }, []);
+  useEffect(() => {
+    getPermissions();
+  }, []);
 
+  permissionAlert = () => {
+    Alert.alert(
+      "Permission Required",
+      "This app need approval to read audio files for local storage",
+      [
+        {
+          test: "I am ready",
+          onPress: () => getPermissions(),
+        },
+        {
+          text: "cancel",
+          onPress: () => getPermissions(),
+        },
+      ]
+    );
+  };
 
-    permissionAlert = () => {
-        Alert.alert("Permission Required", "This app need approval to read audio files for local storage",
-            [{
-                test: "I am ready",
-                onPress: () => this.getPermissions()
-            },
-            {
-                text: "cancel",
-                onPress: () => this.getPermissions()
-            }
-            ])
+  const getAudioFiles = async () => {
+    let media = await MediaLibrary.getAssetsAsync({
+      mediaType: "audio",
+    });
+
+    media = await MediaLibrary.getAssetsAsync({
+      mediaType: "audio",
+      first: media.totalCount,
+    });
+    setAudioFiles(media);
+
+    //console.log(media)
+  };
+
+  const getPermissions = async () => {
+    const permission = await MediaLibrary.getPermissionsAsync();
+
+    if (permission.granted) {
+      //get all audio file
+      getAudioFiles();
     }
 
-    const getAudioFiles = async () => {
-        let media = await MediaLibrary.getAssetsAsync({
-            mediaType: 'audio'
-        });
-
-        media = await MediaLibrary.getAssetsAsync({
-            mediaType: 'audio',
-            first: media.totalCount
-        });
-        setAudioFiles(media);
-
-        //console.log(media)
+    if (!permission.granted && permission.canAskAgain) {
+      const { status, canAskAgain } =
+        await MediaLibrary.requestPermissionsAsync();
+      if (status === "denied" && canAskAgain) {
+        //user must allow the permission
+        permissionAlert();
+      }
+      if (status === "granted") {
+        //get all audio files
+        getAudioFiles();
+      }
+      if (status === "denied" && !canAskAgain) {
+        //display error
+      }
     }
+  };
 
-    const getPermissions = async () => {
-        const permission = await MediaLibrary.getPermissionsAsync()
+  return (
+    <AudioContext.Provider value={{ audioFiles }}>
+      {children}
+    </AudioContext.Provider>
+  );
+};
 
-        if (permission.granted) {
-            //get all audio file
-            getAudioFiles()
-        }
-
-        if (!permission.granted && permission.canAskAgain) {
-            const { status, canAskAgain } = await MediaLibrary.requestPermissionsAsync();
-            if (status === 'denied' && canAskAgain) {
-                //user must allow the permission
-                this.permissionAlert()
-            }
-            if (status === 'granted') {
-                //get all audio files
-                getAudioFiles()
-            }
-            if (status === 'denied' && !canAskAgain) {
-                //display error
-            }
-        }
-    }
-
-
-
-    return (
-        <AudioContext.Provider value={{ audioFiles}}>
-            {children}
-
-        </AudioContext.Provider>
-    )
-}
-
-export default AudioPlayer
+export default AudioPlayer;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { COLOR_BLACK, COLOR_WHITE } from '../../../res/drawables';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { COLOR_BLACK, COLOR_WHITE } from "../../res/drawables";
 
-
-
-export default function Main() {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
 
       <Text style={{ color: COLOR_WHITE }}>This is a mp3player app!</Text>
-
     </View>
   );
-}
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLOR_BLACK,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
